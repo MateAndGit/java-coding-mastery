e85571c21432d39e9e2c8a216e2efa44b8773df4
USER <sola201@naver.com>
2025-10-29 21:54:49 +0900

    chapter2_6_group_with_new_lines

---
+++ src/general/chapter2_6_group_with_new_lines/solution/DistanceUnit.java
@@ -0,0 +1,23 @@
+package general.chapter2_6_group_with_new_lines.solution;
+
+enum DistanceUnit {
+
+    MILES, KILOMETERS;
+
+    static final int IDENTITY = 1;
+
+    static final double MILE_IN_KILOMETERS = 1.60934;
+    static final double KILOMETER_IN_MILES = 1 / MILE_IN_KILOMETERS;
+
+    double getConversionRate(DistanceUnit unit) {
+        if (this == null) {
+            return IDENTITY;
+        }
+
+        if (this == MILES && unit == KILOMETERS) {
+            return MILE_IN_KILOMETERS;
+        } else {
+            return KILOMETER_IN_MILES;
+        }
+    }
+}
