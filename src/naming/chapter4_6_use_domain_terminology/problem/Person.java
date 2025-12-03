19702c86daad39beaf9019a2319bbd700740b0a0
USER <sola201@naver.com>
2025-12-03 17:46:43 -0300

    chapter4_6_use_domain_terminology

---
+++ src/naming/chapter4_6_use_domain_terminology/problem/Person.java
@@ -0,0 +1,25 @@
+package naming.chapter4_6_use_domain_terminology.problem;
+
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+class Problem {
+}
+
+
+class Person {
+    String lastName;
+    String role;
+    int travels;
+    LocalDate employedSince;
+
+    String serializeAsLine() {
+        return String.join(",",
+                Arrays.asList(lastName,
+                        role,
+                        String.valueOf(travels),
+                        String.valueOf(employedSince))
+        );
+    }
+}
---
+++ src/naming/chapter4_6_use_domain_terminology/solution/Person.java
@@ -0,0 +1,26 @@
+package naming.chapter4_6_use_domain_terminology.solution;
+
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+class Solution {
+}
+
+
+class Astronaut {
+    String tagName;
+    String rank;
+    int missions;
+    LocalDate activeDutySince;
+
+    String toCSV() {
+        return String.join(",",
+                Arrays.asList(tagName,
+                        rank,
+                        String.valueOf(missions),
+                        String.valueOf(activeDutySince))
+        );
+    }
+}
+
